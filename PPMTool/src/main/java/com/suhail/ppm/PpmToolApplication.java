de3b6e540348309eaab9d67724db900dc00670f0
Gupta <suhail.gupta@siemens.com>
2018-11-01 10:25:16 +0530

    first-commit

---
+++ PPMTool/src/main/java/com/suhail/ppm/PpmToolApplication.java
@@ -0,0 +1,12 @@
+package com.suhail.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolApplication.class, args);
+	}
+}
