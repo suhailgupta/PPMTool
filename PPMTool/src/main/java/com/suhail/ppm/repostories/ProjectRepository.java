aa2747540e9de98e5bd9c3b4c8ff9bcece32929c
Gupta <suhail.gupta@siemens.com>
2018-11-01 16:47:15 +0530

    Project Repository and Updated Project class

---
+++ PPMTool/src/main/java/com/suhail/ppm/repostories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.suhail.ppm.repostories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.suhail.ppm.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+}
