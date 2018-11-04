7205118c85c8218876a6df5ef1076bb54b0ef1cc
suhailgupta <suhailgupta.007@gmail.com>
2018-11-04 22:22:37 +0530

    Checking in the front-end and back-end changes

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,16 +1,53 @@
 import React, { Component } from "react";
 import ProjectItem from "../components/Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    // const projectObject = {
+    //   projectName: "Project Name PROPS",
+    //   projectIdentifier: "PROPS",
+    //   description: "description from PROPS"
+    // };
+    const { projects } = this.props.project;
     return (
-      <div>
-        <h1 className="alert alert-warning">Welcome to Dashboard</h1>
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-export default Dashboard;
+
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjects }
+)(Dashboard);
